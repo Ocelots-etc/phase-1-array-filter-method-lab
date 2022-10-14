27f4d214441c48568d9aef6b8236bdfb15863c68
catdgomez <catherinedgomez@gmail.com>
2022-10-13 23:26:02 -0500

    set up based on entire lab expectations

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(){
+  //** arguments are an array of drivers' names and a string */
+    
+  //** returns the matching list of drivers */
+  //** case insensitive */
+}
+
+function fuzzyMatch(){
+  //** arguements are an array of drivers' names and a string  */
+  //** returns all drivers whoe names begin with the provided letters */
+}
+
+function matchName(){
+  //** arguments are an array of driver objects and a string as arguments */
+  //** driver object properties: name and hometown */
+  //** returns element if name property matches string argument */
+}
